ffb0f67c75614c8aff90fb266baf4601f94686a9
anh.nguyen <bstar.anh.nguyen@finos.asia>
2023-11-26 16:23:23 +0700

    add module

---
+++ OrientDB/page_rank/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PagerankConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'page_rank'
---
+++ OrientDB/tf_idf/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DfidfConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tf_idf'
